e4370fe647b2b8ab50a59bb252c8a6e542bcdc90
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2025-10-17 19:23:01 +0530

    Show time

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ const body = `{"atheleteID":"${atheleteId}","isActive":true,"date":"${today}","I
 
 fetch(url, {method: 'POST', body, headers: { 'content-type': 'application/json'}}).then(res => res.json()).then((data) => {
     const info = data.data.currentWeekModules[today][0]
-    info.date = today;
+    info.date = date
     fs.writeFileSync('data.json', JSON.stringify(info));
 })
